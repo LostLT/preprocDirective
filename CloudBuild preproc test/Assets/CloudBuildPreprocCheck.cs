e3b9804c58d1f08b4c9ead8e85d5463133b2616d
LostLT <Adam@Administrators-iMac.local>
2014-10-09 14:26:11 -0400

    stuff and things
    things and stuff

---
+++ CloudBuild preproc test/Assets/CloudBuildPreprocCheck.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudBuildPreprocCheck : MonoBehaviour {
+
+	public Texture sophie;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+
+
+
+		#if UNITY_CLOUD_BUILD
+
+		GUI.Label (new Rect(10,10,100,100), "If doge then well-building CloudBuild!");
+		GUI.Label (new Rect(100, 10, 300, 300), sophie);
+			
+
+		#else
+
+		GUI.Label (new Rect(10,10, 100, 100), "This is not built in CloudBuild!");
+
+		#endif
+
+
+
+	}
+}
